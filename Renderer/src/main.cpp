e2d12253807fd244681736a9169792f5dfa6cd6c
sandrohuber <sandrohuber89@hotmail.com>
2018-07-12 22:55:12 +0200

    Added LodePNG library to renderer. Can render test.png 300x300 all white

--- Renderer/src/main.cpp
+++ Renderer/src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "RayTracer/Sphere.h"
+#include "lodepng.h"
 
 int main(int argc, char** argv)
 {
     auto s = Sphere();
     cout << "hey" << endl;
-    
+
+    auto width = 300;
+    auto height = 300;
+    auto imageData = vector<unsigned char>(width*height*4, 255);
+    auto error = lodepng::encode("test.png", imageData, width, height);
+    cout << "error = " << error << endl;
+
     return 0;
 }
