edacb896d1029da5acb4af2801bccdf10fdf113a
sandrohuber <sandrohuber89@hotmail.com>
2018-07-13 17:05:30 +0200

    Added comment related to the ITraceable interface and the non-pure virtual Trace method

--- RayTracer/include/RayTracer/ITraceable.h
+++ RayTracer/include/RayTracer/ITraceable.h
@@ -6,6 +6,11 @@
 class ITraceable
 {
 public:
+    // If this is a pure virtual method, then we can't instantiate a vector of
+    // ITraceables, thus throw. Another possibility is to have a vector of
+    // ITraceable*, but then we lose the very important instance locality when
+    // iterating over the objects. This can happens a lot, because every ray checks
+    // all objects for collisions, shadowing, ect...
     virtual Pixel Trace(const Eigen::Vector3f& origin, const Eigen::Vector3f& direction)
     {
         throw std::runtime_error("Not implemented");
